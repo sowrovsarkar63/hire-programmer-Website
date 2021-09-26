917a1ddbe619c37a27de0ffe904bac12d59aa76c
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-26 11:21:20 +0600

    header section completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <header className="header-area">
+                <h1>
+                    World Class<span className="highlight"> Programmer</span>
+                </h1>
+                <p>
+                    World Is Very Competitive Now A Days. That's Why You Need
+                    WorldClass Programmer
+                </p>
+                <h2>Total Budget 12 Million</h2>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
